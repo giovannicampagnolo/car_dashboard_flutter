67d88e19fe70cb2549c73ce09fcb5007f0079201
Giovanni Campagnolo - Developer <115693598+giovannicampagnolo@users.noreply.github.com>
2022-12-06 10:35:14 +0100

    test: testing size screen

--- lib/hmi_dashboard.dart
+++ lib/hmi_dashboard.dart
@@ -5,6 +5,11 @@ class HmiDashboard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SizedBox(
+        child: (size.width > 1184 && size.height > 604) ? Container() : const Center(child: Text("Screen is too small to UI"),) ,
+      )
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'The Flutter Way',
+      title: 'car dashboard',
       theme: ThemeData.dark(),
       home: const HmiDashboard(),
     );
