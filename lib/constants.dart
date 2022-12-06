bd7f6a014fd29592e955fe7bfa1020bef0ec12d6
Giovanni Campagnolo - Developer <115693598+giovannicampagnolo@users.noreply.github.com>
2022-12-06 10:12:54 +0100

    test: first try

---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+Color primaryColor = const Color(0xFF6B4339);
+const double defaultPadding = 20.0;
---
+++ lib/hmi_dashboard.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HmiDashboard extends StatelessWidget {
+  const HmiDashboard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
